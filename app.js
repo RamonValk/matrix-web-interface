529e54f2b2cadfb5499f332cd1b6a5de5a627a5a
Ramon Valk <valkramon1@gmail.com>
2021-04-24 22:13:55 +0200

    add variations

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.render("index", { title: "Home" });
   console.dir(req.body);
-  shell.ls();
   console.dir(shell.pwd());
   console.dir(
     shell.exec(
       "sudo ./bash/utils/led-image-viewer ./bash/utils/testmedia/aoe.gif --led-cols=64 --led-rows=64"
     )
   );
+
+  const execView = (location) => {
+    console.dir(
+      shell.exec(
+        `sudo ./bash/utils/led-image-viewer ./bash/utils/testmedia/${location} --led-cols=64 --led-rows=64`
+      )
+    );
+  };
+  switch (req.body.mediaType) {
+    case "aoe":
+      execView("geek/aoe.gif");
+      break;
+    case "nyancat":
+      execView("geek/nyancat.gif");
+      break;
+    case "bb8":
+      execView("geek/bb8.gif");
+      break;
+    default
+      console.dir('No such mediaType');
+  }
 });
 app.listen(3000, () => console.log("Listening on port 3000"));
