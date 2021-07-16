cf7c9805336cb6e6b7058f83c399736bfd76b3cb
RamonValk <valkramon1@gmail.com>
2021-07-16 20:20:52 +0200

    new kill

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const execView = (location) => {
   console.log("CurrentViewPID before execute: ", currentViewPID);
   if (currentViewPID) {
     console.log("CurrentViewPID exists: ", currentViewPID);
-    // const execKill = shell.exec(`kill -9 ${currentViewPID}`);
-    const execKill = shell.exit(currentViewPID);
+    const execKill = shell.exec(`kill --SIGTERM -- -${currentViewPID}`);
+    // const execKill = shell.exit(currentViewPID);
     console.log("execKill output: ");
     console.dir(execKill);
   }
